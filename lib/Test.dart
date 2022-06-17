af3a9a27480e467b7bcf2ab6923bc64d44ebc928
rw <vdsavaliya19@gmail.com>
2022-06-17 18:32:06 +0530

    17/06/2022

---
+++ lib/Test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestClass extends StatefulWidget {
+  const TestClass({Key? key}) : super(key: key);
+
+  @override
+  State<TestClass> createState() => _TestClassState();
+}
+
+class _TestClassState extends State<TestClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
